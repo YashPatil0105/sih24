63606f86e2954ebe2ca43066e039906b856234d2
yash patil <yashpatil0107@gmail.com>
2024-08-28 16:00:52 +0530

    added simulator

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-import { CarbonNeutrality, CarbonCredits, Home, Estimation } from "./components/pages";
+import { CarbonNeutrality, CarbonCredits, Home, Estimation,CarbonNeutralitySimulator } from "./components/pages";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/carbonNeutrality" element={<CarbonNeutrality />} />
+        <Route path="/carbonNeutralitySimulator" element={<CarbonNeutralitySimulator />} />
         <Route path="/estimation" element={<Estimation />} />
         <Route path="/carbonCredits" element={<CarbonCredits />} />
       </Routes>
