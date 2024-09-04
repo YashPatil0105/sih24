26fe26fee9e8036952936355692afdf875d8ea35
yash patil <yashpatil0107@gmail.com>
2024-09-05 00:13:13 +0530

    backend integration of estimation

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectDB = (url) => {
-    return mongoose.connect(url);
+const connectDB = (MONGO_URI) => {
+    return mongoose.connect(MONGO_URI);
 };
 
 module.exports = connectDB;
